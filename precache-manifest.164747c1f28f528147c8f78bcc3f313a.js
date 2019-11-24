325327d874ba614e2fc0f0da012f2182cb9621a2
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-24 17:40:47 +0100

    Updates

---
+++ precache-manifest.164747c1f28f528147c8f78bcc3f313a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c33e7a7a93dff785cc9a1f2f37f526",
+    "url": "/watch-alone/index.html"
+  },
+  {
+    "revision": "ed6fb399a5b773bf8b8e",
+    "url": "/watch-alone/static/css/main.7d27cd65.chunk.css"
+  },
+  {
+    "revision": "6504b133df6d10bc6a9b",
+    "url": "/watch-alone/static/js/2.4b07402e.chunk.js"
+  },
+  {
+    "revision": "ed6fb399a5b773bf8b8e",
+    "url": "/watch-alone/static/js/main.a630b104.chunk.js"
+  },
+  {
+    "revision": "0fa06ba006764bcfe1c1",
+    "url": "/watch-alone/static/js/runtime-main.a98a3fbc.js"
+  }
+]);
