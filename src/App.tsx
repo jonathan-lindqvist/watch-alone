66ea7a6df204eb3b2ecdb818156806feda8c6bed
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-24 17:21:39 +0100

    Can now search for video and show it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
 const App: React.FC = () => {
 
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState('')
+  const [videoID, setVideoID] = useState('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    let id = url.split('v=')[1]
+    const ampersandPosition = id.indexOf('&')
+    if(ampersandPosition !== -1) {
+      id = id.substring(0, ampersandPosition)
+    }
+    setVideoID(id)
+  }
 
   return (
     <div>
       <h1>WatchAlone</h1>
       <a href={url}>{url}</a>
-      <input type="text" value={url} onChange={event => setUrl(event.target.value)} />
-      <iframe src={url}></iframe>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={url} onChange={event => setUrl(event.target.value)} />
+        <input type="submit" value="Search"></input>
+      </form>
+      <iframe title="ytFrame" src={`https://www.youtube.com/embed/${videoID}`}></iframe>
     </div>
   );
 }
